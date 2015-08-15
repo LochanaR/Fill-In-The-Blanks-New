f6c9017f60b0ecf7c6c12d436638cbc92bd1c68a
LochanaR <lochanaranaweera@gmail.com>
2015-08-15 22:15:19 +0530

    Rotationzzz :/

---
+++ FITB New/Assets/Scripts/Scores.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Scores : MonoBehaviour {
+	Text score;
+	private int _score;
+	// Use this for initialization
+	void Start () {
+	
+		_score = 0;
+		score = gameObject.GetComponent<Text> ();
+		score.text ="Score" + _score; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Debug.Log(score.text);
+	}
+}
